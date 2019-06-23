6e54601dd06f33c2d59a01bd16933f88b3927f10
MaersTek <maerstek@gmail.com>
2019-06-23 19:34:03 +0300

    Refactored

--- app/src/main/java/com/maerstek/sbx_app/AI.java
+++ app/src/main/java/com/maerstek/sbx_app/AI.java
@@ -7,28 +7,28 @@ import java.util.Map;
 
 public class AI {
 
-    public static String getAnswer(String user_question){
+    public static String getAnswer(String user_question) {
 
         HashMap<String, String> database = new HashMap<String, String>() {{
-            put("hello","Dodou");
-            put("wassup","norm!");
-            put("go to hell","Kiss my shiny metal ass !");
+            put("hello", "Dodou");
+            put("wassup", "norm!");
+            put("go to hell", "Kiss my shiny metal ass !");
         }};
 
         user_question = user_question.toLowerCase();
 
         List<String> answers = new ArrayList<>();
 
-        for (String database_question : database.keySet()){
-            if (user_question.contains(database_question)){
+        for (String database_question : database.keySet()) {
+            if (user_question.contains(database_question)) {
                 answers.add(database.get(database_question));
             }
         }
 
-        if (answers.isEmpty()){
+        if (answers.isEmpty()) {
             answers.add("OK");
         }
 
-       return String.join(",", answers);
+        return String.join(",", answers);
     }
 }
