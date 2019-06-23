72c45d30de04c2da29bcf7a94a60acd8a2f36e35
MaersTek <maerstek@gmail.com>
2019-06-23 19:32:42 +0300

    Developed question|answer core, developed simple logic

---
+++ app/src/main/java/com/maerstek/sbx_app/AI.java
@@ -0,0 +1,34 @@
+package com.maerstek.sbx_app;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class AI {
+
+    public static String getAnswer(String user_question){
+
+        HashMap<String, String> database = new HashMap<String, String>() {{
+            put("hello","Dodou");
+            put("wassup","norm!");
+            put("go to hell","Kiss my shiny metal ass !");
+        }};
+
+        user_question = user_question.toLowerCase();
+
+        List<String> answers = new ArrayList<>();
+
+        for (String database_question : database.keySet()){
+            if (user_question.contains(database_question)){
+                answers.add(database.get(database_question));
+            }
+        }
+
+        if (answers.isEmpty()){
+            answers.add("OK");
+        }
+
+       return String.join(",", answers);
+    }
+}
